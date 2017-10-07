05c14e7a8bab15cdea773679c74ecbf61c8d3e5d
orhaneee <orhankd35@gmail.com>
2017-10-07 07:55:46 +0300

    Server side desktop app, with WPF :fire:

---
+++ CentralDashboard/CentralDashboard/PreloadScreen.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace CentralDashboard
+{
+    /// <summary>
+    /// PreloadScreen.xaml etkileşim mantığı
+    /// </summary>
+    public partial class PreloadScreen
+    {
+        /*
+         * AIzaSyAdoqP3R3iuebZi9SfiQu313MeacdyRtsk
+         */
+
+        public PreloadScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+    }
+}
